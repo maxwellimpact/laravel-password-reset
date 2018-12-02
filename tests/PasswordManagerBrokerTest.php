986587d30824533b8a44031d568ade1601cb6010
Jonathan Cox <joncox@gmail.com>
2018-12-02 12:42:38 -0500

    Add basic integration test

---
+++ tests/PasswordManagerBrokerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Maxwellimpact\PasswordReset\Test;
+
+use Illuminate\Auth\Passwords\DatabaseTokenRepository;
+use Illuminate\Support\Facades\Password;
+use Maxwellimpact\PasswordReset\InMemoryTokenRepository;
+
+class PasswordManagerBrokerTest extends TestCase
+{
+    public function testDefaultRepositoryLoads()
+    {
+        config(['auth.passwords.users' => [
+            'provider' => 'users',
+            'table' => 'password_resets',
+            'expire' => 60,
+        ]]);
+
+        $repository = Password::broker()->getRepository();
+
+        self::assertNotNull($repository);
+        self::assertInstanceOf(DatabaseTokenRepository::class, $repository);
+    }
+
+    public function testCorrectRepositoryLoads()
+    {
+        config(['auth.passwords.users' => [
+            'provider' => 'users',
+            'repository' => 'in_memory',
+            'expire' => 10,
+        ]]);
+
+        $repository = Password::broker()->getRepository();
+
+        self::assertNotNull($repository);
+        self::assertInstanceOf(InMemoryTokenRepository::class, $repository);
+    }
+}
