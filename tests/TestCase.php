596c287aeb2f62ddc976ec40b4f294391d1c7bfe
Jonathan Cox <joncox@gmail.com>
2018-12-02 14:41:09 -0500

    Add faker to the tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,8 @@
 
 namespace Maxwellimpact\PasswordReset\Test;
 
+use Faker\Factory;
+use Faker\Generator;
 use Illuminate\Support\Facades\Password;
 use Maxwellimpact\PasswordReset\InMemoryTokenRepository;
 use Maxwellimpact\PasswordReset\PasswordResetServiceProvider;
@@ -9,9 +11,16 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
+    /**
+     * @var Generator
+     */
+    protected $faker;
+
     protected function setUp()
     {
         parent::setUp();
+
+        $this->faker = Factory::create();
 
         config(['app.key' => 'blahappkeyblah']);
 
